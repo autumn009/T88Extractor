a5fd0008478f02da640363a702e0fae5c4637413
Akira Kawamata <autumn@piedey.co.jp>
2022-09-18 17:03:54 +0900

    add usage

---
+++ T88Extractor/T88Extractor/Program.cs
@@ -0,0 +1,18 @@
+﻿if( args.Count() == 0)
+{
+    usage();
+    return;
+}
+
+
+
+
+
+
+void usage()
+{
+    Console.WriteLine("T88Extgractor Path... [-list] [-override]");
+    Console.WriteLine("Path: wild card allowed");
+    Console.WriteLine("-list: list only");
+    Console.WriteLine("-override: erase directory before run");
+}
