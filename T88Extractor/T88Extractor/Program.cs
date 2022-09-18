69ab469545dd8418e2478f7b5811340f0b0d49c5
Akira Kawamata <autumn@piedey.co.jp>
2022-09-18 17:10:53 +0900

    create file list

--- T88Extractor/T88Extractor/Program.cs
+++ T88Extractor/T88Extractor/Program.cs
@@ -4,9 +4,27 @@
     return;
 }
 
-
-
-
+var listTargets = new List<string>();
+bool listFlag = false;
+bool overrideFlag = false;
+foreach (var target in args)
+{
+    if(target == "-list") listFlag = true;
+    else if (target == "-override") overrideFlag = true;
+    else
+    {
+        var dir = Path.GetDirectoryName(target)?? Directory.GetCurrentDirectory();
+        var name = Path.GetFileName(target);
+        foreach (var item in Directory.GetFiles(dir,name))
+        {
+            listTargets.Add(item);
+        }
+    }
+}
+foreach (var item in listTargets)
+{
+    Console.WriteLine(item);
+}
 
 
 void usage()
