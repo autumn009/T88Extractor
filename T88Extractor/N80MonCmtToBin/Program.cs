91032842069283febc3e81a7bcc21a6626ffd592
Akira Kawamata <autumn@piedey.co.jp>
2022-09-25 10:07:19 +0900

    add N80MonCmtToBin

---
+++ T88Extractor/N80MonCmtToBin/Program.cs
@@ -0,0 +1,30 @@
+﻿// convert PC-8001 N-Basic monitor saved file to binary
+
+if( args.Length != 2)
+{
+    Console.WriteLine("usage: N80MonCmtToBun SRC_FILE_NAME DST_FILE_NAME");
+    return;
+}
+
+using var streamR = File.OpenRead(args[0]);
+using var streamW = File.OpenWrite(args[1]);
+
+streamR.ReadByte();    // skip 3a
+var hi = streamR.ReadByte();
+var lo = streamR.ReadByte();
+streamR.ReadByte();    // skip checksum
+Console.WriteLine($"Start address: {hi:X2}{lo:X2}");
+
+for (; ;)
+{
+    streamR.ReadByte();    // skip 3a
+    var count = streamR.ReadByte();
+    if (count <= 0) break;
+    for (int i = 0; i < count; i++)
+    {
+        var d = streamR.ReadByte();
+        streamW.WriteByte((byte)d);
+    }
+    streamR.ReadByte();    // skip checksum
+}
+Console.WriteLine("Done");
